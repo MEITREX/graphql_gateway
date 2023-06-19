6004ab068009b50e7d88c09a1fc68b7031434ea5
Valentin Morlock <valentin@morlock.io>
2023-06-19 19:36:42 +0200

    fix auth

--- envelopPlugins.ts
+++ envelopPlugins.ts
@@ -1,54 +1,51 @@
-import { PluginOrDisabledPlugin } from '@envelop/core';
-import { useGenericAuth, ResolveUserFn } from '@envelop/generic-auth';
-
+import { PluginOrDisabledPlugin } from "@envelop/core";
+import { ResolveUserFn, useGenericAuth } from "@envelop/generic-auth";
+import * as jose from "jose";
 const keycloakHost = "host.docker.internal";
 const keycloakPort = "9009";
 const realmName = "GITS";
 
 type UserType = {
-    id: string
-}
-   
-const resolveUserFn: ResolveUserFn<UserType> = async context => {
-    // Here you can implement any custom sync/async code, and use the context built so far in Envelop and the HTTP request
-    // to find the current user.
-    // Common practice is to use a JWT token here, validate it, and use the payload as-is, or fetch the user from an external services.
-    // Make sure to either return `null` or the user object.
+  id: string;
+};
 
-    try {
-        // get user information from request headers
-        const authHeader = context.req.headers.authorization;
+const JWKS = jose.createRemoteJWKSet(
+  new URL(
+    `http://${keycloakHost}:${keycloakPort}/realms/${realmName}/protocol/openid-connect/certs`
+  )
+);
 
-        const url = `http://${keycloakHost}:${keycloakPort}/realms/${realmName}/protocol/openid-connect/userinfo`;
-        const options = {
-            method: "GET",
-            headers: {
-                "authorization": authHeader
-            }
-        };
+const resolveUserFn: ResolveUserFn<UserType> = async (context) => {
+  // Here you can implement any custom sync/async code, and use the context built so far in Envelop and the HTTP request
+  // to find the current user.
+  // Common practice is to use a JWT token here, validate it, and use the payload as-is, or fetch the user from an external services.
+  // Make sure to either return `null` or the user object.
 
-        // send request to keycloak
-        const response = await fetch(url, options);
-        const responseJson = await response.json();
+  try {
+    // get user information from request headers
+    const authHeader = context.req.headers.authorization.replace("Bearer ", "");
 
-        if (response.status == 200) {
-            return responseJson;
-        } else {
-            return null;
-        }
-    } catch (e) {
-        console.error('Failed to validate token');
-        console.error(e);
+    const { payload, protectedHeader } = await jose.jwtVerify(
+      authHeader,
+      JWKS,
+      {}
+    );
 
-        return null;
-    }
-}
+    return { id: payload.sub };
+    console.log(payload);
+  } catch (e) {
+    console.error("Failed to validate token");
+    console.error(e);
+
+    return null;
+  }
+};
 
 const plugins: PluginOrDisabledPlugin = [
-    useGenericAuth({
-        resolveUserFn,
-        mode: "protect-all"
-    })
-]
+  useGenericAuth({
+    resolveUserFn,
+    mode: "protect-all",
+  }),
+];
 
 export default plugins;
