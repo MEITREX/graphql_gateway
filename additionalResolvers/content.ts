0a03e5cfc66027f5f8dae0db8632a5f80928a413
Lukas <lukas636@live.com>
2023-07-26 09:39:19 +0200

    Improve mediaservice API (#28)
    * Fix bug resolving content ids and move type to more appropriate file
    
    * add custom resolvers for media
    
    * createMediaContentAndLinkRecords mnutation
    
    * content.ts: Remove forgotten debug log statement

---
+++ additionalResolvers/content.ts
@@ -0,0 +1,34 @@
+import { Resolvers } from "../.mesh";
+
+const resolvers: Resolvers = {
+    Mutation: {
+        createMediaContentAndLinkRecords: {
+            async resolve(root, _args, context, info) {
+                // create the content object using the createMediaContent query of the content service
+                let content = await context.ContentService.Mutation.createMediaContent({
+                    root,
+                    args: {
+                        input: _args.contentInput
+                    },
+                    context,
+                    info
+                });
+
+                // link the created content to the passed media records
+                await context.MediaService.Mutation.linkMediaRecordsWithContent({
+                    root,
+                    args: {
+                        contentId: content.id,
+                        mediaRecordIds: _args.mediaRecordIds
+                    },
+                    context,
+                    info
+                });
+
+                return content;
+            }
+        }
+    }
+}
+
+export default resolvers;
