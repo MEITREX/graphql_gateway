977ac97de6e4599385669bf212d43b6d94ecfb80
Lukas <lukas636@live.com>
2023-09-12 15:50:19 +0200

    include course membership data in transmitted user data (#54)
    * include course membership data in transmitted user data
    
    * add comments

--- envelopPlugins.ts
+++ envelopPlugins.ts
@@ -2,12 +2,18 @@ import {PluginOrDisabledPlugin} from "@envelop/core";
 import {ResolveUserFn, useGenericAuth} from "@envelop/generic-auth";
 import * as jose from "jose";
 
+type UserCourseMembership = {
+  courseId: string;
+  role: string;
+}
+
 type UserType = {
   id: string;
   userName: string;
   firstName: string;
   lastName: string;
   authToken: string;
+  courseMemberships: UserCourseMembership[];
 };
 
 const JWKS = jose.createRemoteJWKSet(
@@ -29,14 +35,70 @@ async function resolveUserAuthenticated(context) {
         {}
     );
 
+    // query the user service to find out which courses the user is a member of
+    let userInfoRes = await context.UserService.Query.findUserInfos({
+      args: {
+        ids: [payload.sub]
+      },
+      selectionSet: `
+      {
+        courseMemberships {
+          courseId
+          role
+        }
+      }
+      `
+    });
+
+    // check that we received a response
+    if(userInfoRes.length < 1) {
+      console.error("Failed to retrieve user course memberships.");
+      return null;
+    }
+
+    // query the course service to fetch additional course information for the courses the user is a member of
+    let courseInfoRes = await context.CourseService.Query.coursesByIds({
+      args: {
+        ids: userInfoRes[0].courseMemberships.map((membership) => membership.courseId)
+      },
+      selectionSet: `
+      {
+        id
+        published
+        startDate
+        endDate
+      }
+      `
+    });
+
+    // check that we got information for all courses we requested
+    let missingCourseCount = userInfoRes[0].courseMemberships.length - courseInfoRes.length;
+    if(missingCourseCount > 0) {
+      console.error("Failed to retrieve course information for " + missingCourseCount + " course(s).");
+      return null;
+    }
+
+    // construct the user object we will return
     let user: UserType = {
       id: payload.sub,
       userName: payload.preferred_username,
       firstName: payload.given_name,
       lastName: payload.family_name,
-      authToken: authHeader
+      authToken: authHeader,
+      courseMemberships: userInfoRes[0].courseMemberships.map((membership) => {
+        let courseInfo = courseInfoRes.find((course) => course.id === membership.courseId);
+        return {
+          courseId: membership.courseId,
+          role: membership.role,
+          published: courseInfo.published,
+          startDate: courseInfo.startDate,
+          endDate: courseInfo.endDate
+        }
+      })
     };
 
+    // add json representation of the user object to the context 
+    // (will be injected into the request headers to the services)
     context.currentUserJson = JSON.stringify(user);
 
     return user;
@@ -48,24 +110,9 @@ async function resolveUserAuthenticated(context) {
   }
 }
 
-function resolveUserSkipAuthDangerous(context) {
+function resolveUserSkipAuth(context) {
   return JSON.parse(retrieveHeaderSafe(context, "currentUser"));
 }
-
-const resolveUserFn: ResolveUserFn<UserType> = async (context) => {
-  if(process.env.SKIP_AUTH !== undefined && process.env.SKIP_AUTH.toLowerCase() === "true") {
-    return resolveUserSkipAuthDangerous(context);
-  } else {
-    return await resolveUserAuthenticated(context);
-  }
-};
-
-const plugins: PluginOrDisabledPlugin = [
-  useGenericAuth({
-    resolveUserFn,
-    mode: "protect-all",
-  }),
-];
 
 function retrieveHeadersSafe(context: any) {
   let headers = context.request.headers;
@@ -94,5 +141,28 @@ function retrieveHeaderSafe(context, header: string) {
   }
   return authHeader;
 }
+
+/**
+ * Function which is called for every request to resolve the current user based on data in the request.
+ * @param context The GraphQL context
+ * @returns Object representing the current user or null if authentication failed
+ */
+const resolveUserFn: ResolveUserFn<UserType> = async (context) => {
+  if(process.env.SKIP_AUTH !== undefined && process.env.SKIP_AUTH.toLowerCase() === "true") {
+    return resolveUserSkipAuth(context);
+  } else {
+    return await resolveUserAuthenticated(context);
+  }
+};
+
+/**
+ * List of Envelop plugins the gateway shall use.
+ */
+const plugins: PluginOrDisabledPlugin = [
+  useGenericAuth({
+    resolveUserFn,
+    mode: "protect-all",
+  }),
+];
 
 export default plugins;
