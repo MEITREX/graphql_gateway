dc2ee074b6902f9a2aec499a5c1f023ee61a71f2
Aaron Schmid <56757286+Aaron2000119@users.noreply.github.com>
2025-09-30 11:13:44 +0200

    fix comments

--- additionalResolvers/submission.ts
+++ additionalResolvers/submission.ts
@@ -7,7 +7,7 @@ const resolvers: Resolvers = {
         addTask: {
             /**
              * @param root { assessmentId: <UUID> } ???
-             * @param _args GraqhQL function parameters defined in flashcard.graphqls file
+             * @param _args GraqhQL function parameters defined in submissions.graphqls file
              * @param context Browser context (https request headers, etc.)
              * @param info GraphQL info/ introspection object
              * @param params logger, currentUser, query, token, service objects, etc.
@@ -29,7 +29,7 @@ const resolvers: Resolvers = {
         updateTask: {
             /**
              * @param root { assessmentId: <UUID> } ???
-             * @param _args GraqhQL function parameters defined in flashcard.graphqls file
+             * @param _args GraqhQL function parameters defined in submissions.graphqls file
              * @param context Browser context (https request headers, etc.)
              * @param info GraphQL info/ introspection object
              * @param params logger, currentUser, query, token, service objects, etc.
